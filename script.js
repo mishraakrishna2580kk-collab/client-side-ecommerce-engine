c91425311cbe039bc23897f26c03e0205549d127
Krishna Kumar <mishraakrishna2580kk@gmail.com>
2025-12-31 01:35:07 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -77,8 +77,7 @@ document.addEventListener("DOMContentLoaded", function () {
   message += `\n💰 Total: ₹${totalEl.textContent}`;
 
   const whatsappURL =
-    `https://wa.me/917366076190?text=${encodeURIComponent(message)}`;
-
+    `https://wa.me/7366076190?text=${encodeURIComponent(message)}`;
   window.open(whatsappURL, "_blank");
 };
 
